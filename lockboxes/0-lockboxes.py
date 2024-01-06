bb618826396029f31919c48255de392f21d7b51f
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-05 22:02:41 -0400

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 
 '''
-determines if all the boxes can be opened
+Determines if all the boxes can be opened.
 '''
 
-def canUnlockAll(boxes):
-    boxesOpened = [False] * len(boxes)
-    boxesOpened = openBox(0, boxes, boxesOpened)
 
-    return all(boxesOpened)
+def can_unlock_all(boxes):
+    boxes_opened = [False] * len(boxes)
+    boxes_opened = open_box(0, boxes, boxes_opened)
 
-def openBox(boxIndex, boxes, boxesOpened):
-    if boxIndex < len(boxesOpened) and not boxesOpened[boxIndex]:
-        boxesOpened[boxIndex] = True
-        for var in boxes[boxIndex]:
-            openBox(var, boxes, boxesOpened)
+    return all(boxes_opened)
 
-    return boxesOpened
+
+def open_box(box_index, boxes, boxes_opened):
+    if 0 <= box_index < len(boxes_opened) and not boxes_opened[box_index]:
+        boxes_opened[box_index] = True
+        for var in boxes[box_index]:
+            open_box(var, boxes, boxes_opened)
+
+    return boxes_opened
