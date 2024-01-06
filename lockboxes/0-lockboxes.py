410c29766e1e3223a703ade0cc42d303340a4111
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-05 22:08:37 -0400

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -13,12 +13,10 @@ def canUnlockAll(boxes):
 
     return all(boxesOpened)
 
-
 def openBox(boxIndex, boxes, boxesOpened):
     if boxIndex < len(boxesOpened) and not boxesOpened[boxIndex]:
         boxesOpened[boxIndex] = True
         for var in boxes[boxIndex]:
             openBox(var, boxes, boxesOpened)
             
-
     return boxesOpened
