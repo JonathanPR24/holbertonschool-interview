25a5a1a6bb34e4a1b06fad80821a85af97a91910
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-05 22:01:45 -0400

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,45 +1,19 @@
 #!/usr/bin/python3
 
+'''
+determines if all the boxes can be opened
+'''
+
 def canUnlockAll(boxes):
-    """
-    Determines whether all the boxes can be opened.
+    boxesOpened = [False] * len(boxes)
+    boxesOpened = openBox(0, boxes, boxesOpened)
 
-    Parameters:
-    - boxes (List[List[int]]): A list of lists representing the boxes and keys.
+    return all(boxesOpened)
 
-    Returns:
-    - bool: True if all boxes can be opened, else False.
-    """
-    # Initialize a list to track the state of box unlocking
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True  # The first box is initially unlocked
+def openBox(boxIndex, boxes, boxesOpened):
+    if boxIndex < len(boxesOpened) and not boxesOpened[boxIndex]:
+        boxesOpened[boxIndex] = True
+        for var in boxes[boxIndex]:
+            openBox(var, boxes, boxesOpened)
 
-    changed = True
-
-    # Perform systematic exploration until no more changes can be made
-    while changed:
-        changed = False
-
-        # Iterate through all boxes
-        for i in range(len(boxes)):
-            if not unlocked[i]:
-                continue
-
-            # Explore keys in the current box
-            for j in range(len(boxes[i])):
-                if boxes[i][j] >= len(boxes):
-                    continue
-
-                key = boxes[i][j]
-
-                # Check if the corresponding box is not already unlocked
-                if not unlocked[key]:
-                    unlocked[key] = True
-                    changed = True
-
-    # Check if all boxes are unlocked
-    return all(unlocked)
-
-# Example usage
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
+    return boxesOpened
