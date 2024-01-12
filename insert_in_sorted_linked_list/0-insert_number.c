aee3bc3fd4f437f7599279f2d3a2c75815000321
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-12 18:25:32 -0400

    Insert in sorted linked list

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -1,35 +1,39 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_node - Inserts a node at the correct position in a sorted linked list
- * @head: Pointer to the pointer of the head of the list
- * @number: The value to assign to the new node
- * Return: Pointer to the new node, or NULL on failure
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to the pointer of the head of the list
+ * @number: number to insert
+ * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    if (!head)
-        return NULL;
+    listint_t *new_node, *current;
 
-    listint_t *new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return NULL;
+    if (head == NULL)
+        return (NULL);
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
 
     new_node->n = number;
+    new_node->next = NULL;
 
-    if (!*head || number < (*head)->n)
+    if (*head == NULL || number < (*head)->n)
     {
         new_node->next = *head;
         *head = new_node;
-        return new_node;
+        return (new_node);
     }
 
-    listint_t *current = *head;
-    while (current->next && number >= current->next->n)
+    current = *head;
+    while (current->next != NULL && current->next->n < number)
         current = current->next;
 
     new_node->next = current->next;
     current->next = new_node;
 
-    return new_node;
+    return (new_node);
 }
