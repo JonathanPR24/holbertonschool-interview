fd915dbd47bbe02f994488d86fc95d7ba14b9678
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-22 12:55:21 -0400

    Minimum Operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,16 +4,6 @@ Calculates the minimum operations
 """
 
 def minOperations(n):
-    """
-    Calculate the fewest number of operations needed to reach exactly n H characters.
-
-    Args:
-    - n (int): Target number of H characters.
-
-    Returns:
-    - int: The fewest number of operations needed.
-           If n is impossible to achieve, returns 0.
-    """
     answer = 0
     x = 2
     while n > 1:
@@ -22,11 +12,3 @@ def minOperations(n):
             n /= x
         x += 1
     return answer
-
-# Example usage for testing
-if __name__ == "__main__":
-    n1 = 4
-    print("Min # of operations to reach {} char: {}".format(n1, minOperations(n1)))
-
-    n2 = 12
-    print("Min # of operations to reach {} char: {}".format(n2, minOperations(n2)))
