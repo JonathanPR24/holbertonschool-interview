4d9a5a6e405653bdef1c1a11f646e3021e1343b4
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-30 14:38:22 -0400

    Sandpiles sum

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -10,7 +10,8 @@ static void print_grid(int grid[3][3])
     int i, j;
 
     for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+        for (j = 0; j < 3; j++)
+        {
             if (j)
                 printf(" ");
             printf("%d", grid[i][j]);
@@ -27,7 +28,7 @@ static void print_grid(int grid[3][3])
  */
 void add(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+int i, j;
 
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++)
