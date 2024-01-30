ffdb504c2e7f83bf390bf382d9dc05a5095f9636
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-30 14:20:57 -0400

    Sandpiles sum h file

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+void add(int grid1[3][3], int grid2[3][3]);
+void topple(int grid[3][3], int tmp[3][3]);
+int stable(int grid[3][3]);
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
