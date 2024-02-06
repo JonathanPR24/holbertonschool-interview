d4e0adfe4398d74d7d7be48f69c55325176e1478
Jonathan Perez <jonathanperez604@gmail.com>
2024-02-05 20:50:06 -0400

    Linked list palindrome

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,29 +1,37 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdlib.h>
 
 /**
  * check_recursively - Recursively checks if a linked list is a palindrome
- * @start: Points to the start of the list
- * @end: Points to the end of the list (updated recursively)
+ * @start: Points to the current node in the first half of the list
+ * @end: Points to the current node in the second half of the list (updated recursively)
  *
  * Return: 1 if it is a palindrome, 0 otherwise
  */
 int check_recursively(listint_t **start, listint_t *end)
 {
+    int is_palindrome;
+
     /* Base case: Reached the end of the list, it's a palindrome */
     if (end == NULL)
         return 1;
 
-    /* Compare values at start and end pointers */
-    if ((*start)->n != end->n)
+    /* Check recursively for palindrome in the remaining nodes */
+    is_palindrome = check_recursively(start, end->next);
+
+    /* If previous checks failed, no need to proceed further */
+    if (is_palindrome == 0)
         return 0;
 
-    /* Move start pointer forward, and end pointer backward */
+    /* Compare values at start and end nodes */
+    if ((*start)->n == end->n)
+        is_palindrome = 1;
+    else
+        is_palindrome = 0;
+
+    /* Move start pointer to the next node in the first half */
     *start = (*start)->next;
 
-    /* Continue checking recursively */
-    return check_recursively(start, end->next);
+    return is_palindrome;
 }
 
 /**
@@ -34,16 +42,13 @@ int check_recursively(listint_t **start, listint_t *end)
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t **start;
-    listint_t *end;
+    listint_t **start = head;
+    listint_t *end = *head;
 
-    /* Empty list is considered a palindrome */
+    /* An empty list is always considered a palindrome */
     if (*head == NULL)
         return 1;
 
-    start = head;
-    end = *head;
-
-    /* Check recursively for palindrome */
+    /* Check recursively for palindrome starting from both ends */
     return check_recursively(start, end);
 }
