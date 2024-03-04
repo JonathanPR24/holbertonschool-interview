bb85672546519c641cbc69dd56f87849f4a53b41
Jonathan <jonathanperez604@gmail.com>
2024-03-04 19:12:57 -0400

    UTF-8 Validation

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,29 +1,43 @@
 #!/usr/bin/python3
 
+""" 
+utf8_validation
+
+This module provides a function to determine if a given data set represents
+a valid UTF-8 encoding.
+
+Example:
+    data = [65, 195, 128, 226, 152, 131]
+    print(validUTF8(data))  # Output: True
+"""
+
 def validUTF8(data):
-    # Number of bytes to check for each UTF-8 character
+    """ 
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data (list): A list of integers representing bytes of data.
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, False otherwise.
+    """
     num_bytes_to_check = 0
 
     for num in data:
-        # If num_bytes_to_check is 0, we are expecting the start of a new UTF-8 character
         if num_bytes_to_check == 0:
-            # Count the number of leading 1s to determine the number of bytes in the UTF-8 character
             if num >> 5 == 0b110:
                 num_bytes_to_check = 1
             elif num >> 4 == 0b1110:
                 num_bytes_to_check = 2
             elif num >> 3 == 0b11110:
                 num_bytes_to_check = 3
-            # If num_bytes_to_check is still 0, it means it's either a single byte character or an invalid character
             elif num >> 7 == 0:
                 num_bytes_to_check = 0
             else:
                 return False
         else:
-            # Check if the current number starts with the correct bit pattern
             if num >> 6 != 0b10:
                 return False
             num_bytes_to_check -= 1
 
-    # If num_bytes_to_check is not zero at the end, it means some characters are incomplete
     return num_bytes_to_check == 0
