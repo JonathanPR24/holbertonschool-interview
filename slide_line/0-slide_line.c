de9df636ea4755380373cfd70fb92f072fcf2539
Jonathan <jonathanperez604@gmail.com>
2024-03-18 18:26:49 -0400

    Slide line

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,44 +1,21 @@
 #include "slide_line.h"
+#include <stdio.h>
 
 /**
- * slide_right - Slide and merge an array of integers to the right direction
+ * slide_line - Slide and merge an array of integers to the specified direction
  * @line: Pointer to the array of integers
  * @size: Number of elements in the array
+ * @direction: Direction of the slide operation (SLIDE_LEFT or SLIDE_RIGHT)
  * Return: 1 upon success, 0 upon failure
  */
-int slide_right(int *line, size_t size)
+int slide_line(int *line, size_t size, int direction)
 {
-    size_t i, j;
-    int merged = 0;
-
-    for (i = size - 1; i < size; i--) {
-        if (line[i] == 0)
-            continue;
-
-        for (j = i - 1;; j--) {
-            if (line[j] == 0 && j != 0)
-                continue;
-            if (line[i] == line[j]) {
-                line[i] *= 2;
-                line[j] = 0;
-                merged = 1;
-            }
-            break;
-        }
-    }
-
-    for (i = size - 1, j = size - 1; i < size; i--) {
-        if (line[i] != 0) {
-            if (i != j) {
-                line[j] = line[i];
-                line[i] = 0;
-                merged = 1;
-            }
-            j--;
-        }
-    }
-
-    return merged;
+    if (direction == SLIDE_RIGHT)
+        return slide_right(line, size);
+    else if (direction == SLIDE_LEFT)
+        return slide_left(line, size);
+    else
+        return 0; // Invalid direction
 }
 
 /**
@@ -79,20 +56,52 @@ int slide_left(int *line, size_t size)
         }
     }
 
+    if (!merged)
+        fprintf(stderr, "Failed to slide and merge line\n");
+
     return merged;
 }
 
 /**
- * slide_line - Slide and merge an array of integers to the specified direction
+ * slide_right - Slide and merge an array of integers to the right direction
  * @line: Pointer to the array of integers
  * @size: Number of elements in the array
- * @direction: Direction of the slide operation (SLIDE_LEFT or SLIDE_RIGHT)
  * Return: 1 upon success, 0 upon failure
  */
-int slide_line(int *line, size_t size, int direction)
+int slide_right(int *line, size_t size)
 {
-    if (direction == SLIDE_RIGHT)
-        return slide_right(line, size);
-    else
-        return slide_left(line, size);
+    size_t i, j;
+    int merged = 0;
+
+    for (i = size - 1; i < size; i--) {
+        if (line[i] == 0)
+            continue;
+
+        for (j = i - 1;; j--) {
+            if (line[j] == 0 && j != 0)
+                continue;
+            if (line[i] == line[j]) {
+                line[i] *= 2;
+                line[j] = 0;
+                merged = 1;
+            }
+            break;
+        }
+    }
+
+    for (i = size - 1, j = size - 1; i < size; i--) {
+        if (line[i] != 0) {
+            if (i != j) {
+                line[j] = line[i];
+                line[i] = 0;
+                merged = 1;
+            }
+            j--;
+        }
+    }
+
+    if (!merged)
+        fprintf(stderr, "Failed to slide and merge line\n");
+
+    return merged;
 }
--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -3,9 +3,11 @@
 
 #include <stddef.h>
 
-#define SLIDE_LEFT  (-1)
-#define SLIDE_RIGHT (1)
+#define SLIDE_LEFT  0
+#define SLIDE_RIGHT 1
 
 int slide_line(int *line, size_t size, int direction);
+int slide_left(int *line, size_t size);
+int slide_right(int *line, size_t size);
 
 #endif /* SLIDE_LINE_H */
