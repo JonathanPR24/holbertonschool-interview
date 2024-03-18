137ab355b2768c708d630a9720eb82e7f0a7cf09
Jonathan <jonathanperez604@gmail.com>
2024-03-18 18:19:28 -0400

    Slide line

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,22 +1,44 @@
 #include "slide_line.h"
 
-/* Function prototypes */
-int slide_left(int *line, size_t size);
-int slide_right(int *line, size_t size);
-
 /**
- * slide_line - Slide and merge an array of integers to the specified direction
+ * slide_right - Slide and merge an array of integers to the right direction
  * @line: Pointer to the array of integers
  * @size: Number of elements in the array
- * @direction: Direction of the slide operation (SLIDE_LEFT or SLIDE_RIGHT)
  * Return: 1 upon success, 0 upon failure
  */
-int slide_line(int *line, size_t size, int direction)
+int slide_right(int *line, size_t size)
 {
-    if (direction == SLIDE_RIGHT)
-        return slide_right(line, size);
-    else
-        return slide_left(line, size);
+    size_t i, j;
+    int merged = 0;
+
+    for (i = size - 1; i < size; i--) {
+        if (line[i] == 0)
+            continue;
+
+        for (j = i - 1;; j--) {
+            if (line[j] == 0 && j != 0)
+                continue;
+            if (line[i] == line[j]) {
+                line[i] *= 2;
+                line[j] = 0;
+                merged = 1;
+            }
+            break;
+        }
+    }
+
+    for (i = size - 1, j = size - 1; i < size; i--) {
+        if (line[i] != 0) {
+            if (i != j) {
+                line[j] = line[i];
+                line[i] = 0;
+                merged = 1;
+            }
+            j--;
+        }
+    }
+
+    return merged;
 }
 
 /**
@@ -61,42 +83,16 @@ int slide_left(int *line, size_t size)
 }
 
 /**
- * slide_right - Slide and merge an array of integers to the right direction
+ * slide_line - Slide and merge an array of integers to the specified direction
  * @line: Pointer to the array of integers
  * @size: Number of elements in the array
+ * @direction: Direction of the slide operation (SLIDE_LEFT or SLIDE_RIGHT)
  * Return: 1 upon success, 0 upon failure
  */
-int slide_right(int *line, size_t size)
+int slide_line(int *line, size_t size, int direction)
 {
-    size_t i, j;
-    int merged = 0;
-
-    for (i = size - 1; i < size; i--) {
-        if (line[i] == 0)
-            continue;
-
-        for (j = i - 1;; j--) {
-            if (line[j] == 0 && j != 0)
-                continue;
-            if (line[i] == line[j]) {
-                line[i] *= 2;
-                line[j] = 0;
-                merged = 1;
-            }
-            break;
-        }
-    }
-
-    for (i = size - 1, j = size - 1; i < size; i--) {
-        if (line[i] != 0) {
-            if (i != j) {
-                line[j] = line[i];
-                line[i] = 0;
-                merged = 1;
-            }
-            j--;
-        }
-    }
-
-    return merged;
+    if (direction == SLIDE_RIGHT)
+        return slide_right(line, size);
+    else
+        return slide_left(line, size);
 }
